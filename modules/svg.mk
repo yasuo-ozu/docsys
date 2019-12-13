ef209b3990e5530ed413ec92081e31866fe31b01
yozu <yasuo@ozu.email>
2019-12-13 17:37:04 +0900

    reduce commented lines

--- modules/svg.mk
+++ modules/svg.mk
@@ -4,10 +4,7 @@ SOURCES_pdf+=svg
 
 %.png:	%.svg
 	convert -units PixelsPerInch -density 72x72 $< $@
-#svg/%.eps:	svg/%.svg
-#	convert -units PixelsPerInch -density 72x72 $< $@
+	
 %.pdf:	%.svg
-	#convert -units PixelsPerInch -density 72x72 $< $@
-	#convert $< $@
 	inkscape -f $< -A $@
 
