d5ad9a77dfacf6626bb5eda873619df932b053bf
yozu <yasuo@ozu.email>
2019-12-17 10:29:47 +0900

    Adopt to map file and bib file

--- modules/pdf.mk
+++ modules/pdf.mk
@@ -13,29 +13,36 @@ SOURCES_out+=tex
 SOURCES_snm+=tex
 SOURCES_toc+=tex
 
+DVIPDFM_BIN=dvipdfmx
+TEX_MAPFILE=cid-x.map
+
+ifeq (,$(wildcard $(CURDIR)/$(TEX_MAPFILE)))
 %.pdf:	%.dvi $(SYSDIR)
-	cd $(dir $<); dvipdfmx $(notdir $(basename $<)) 2>&1
+	$(DVIPDFM_BIN) -o $@ $(basename $<) 2>&1
+else
+%.pdf:	%.dvi $(TEX_MAPFILE) $(SYSDIR)
+	$(DVIPDFM_BIN) -f $(TEX_MAPFILE) -o $@ $(basename $<) 2>&1
+endif
 	cd $(dir $<); qpdf --linearize $(notdir $@) linearized_$(notdir $@)
 	rm -f $@
 	cd $(dir $<); mv linearized_$(notdir $@) $(notdir $@)
-	
-ifeq (,$(wildcard $(dir $<)/reference.bib))
-reference.bib:
-	touch $@
-endif
 
 TEX_BIN?=platex
 TEX_FLAGS?=--shell-escape -interaction=batchmode -halt-on-error
 PBIBTEX_BIN?=pbibtex
 
 .SECONDARY:	%.dvi
+ifeq (,$(wildcard $(CURDIR)/reference.bib))
+%.dvi:	%.tex $(SYSDIR)
+else
 %.dvi:	%.tex reference.bib $(SYSDIR)
+endif
 	@echo $(TEX_BIN) $(TEX_FLAGS) $<
 	@cd $(dir $<); $(TEX_BIN) $(TEX_FLAGS) $(notdir $<) 1>/dev/null; if [ ! -f $(notdir $(basename $<)).dvi ]; then\
 		cat $(notdir $(basename $<)).log | grep -e "^!" -A 10 1>&2 ;\
 		false ;\
 	fi
-ifeq (,$(shell [ -s $(CURDIR)/reference.bib ] || echo n))
+ifneq (,$(wildcard $(CURDIR)/reference.bib))
 	cd $(dir $<); $(PBIBTEX_BIN) $(notdir $(basename $<))
 	cd $(dir $<); $(TEX_BIN) $(TEX_FLAGS) $(notdir $<)  &>/dev/null
 endif
