98c4500bedc5feca6b09ea8d978e450b1cdf17b0
yozu <yasuo@ozu.email>
2020-12-13 17:52:19 +0900

    Fix saty rule

--- modules/saty.mk
+++ modules/saty.mk
@@ -2,11 +2,13 @@ TARGET_SUFFIXES+=pdf d osaty
 SOURCES_pdf+=saty
 SOURCES_d+=saty
 
-%.pdf:	%.osaty
-	SATYROGRAPHOS_EXPERIMENTAL=1 satyrographos satysfi -- -o $@ $<
+%.pdf:	%.saty
+	eval $(opam env)
+	env SATYROGRAPHOS_EXPERIMENTAL=1 satyrographos satysfi -- -o $@ $<
 	
 .SECONDARY:	%.d
 %.d:	%.saty
-	SATYROGRAPHOS_EXPERIMENTAL=1 satyrographos util deps -r -p --depfile $@ --mode pdf -o "$(basename $@)" $<
+	eval $(opam env)
+	env SATYROGRAPHOS_EXPERIMENTAL=1 satyrographos util deps -r -p --depfile $@ --mode pdf -o "$(basename $@)" $<
 	
 -include $(patsubst %.saty,%.d,$(wildcard ./*.saty) $(wildcard ./**/*.saty))
